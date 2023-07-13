19254ce6c51ffb5fdcd160e01946e21fd537f34e
Isaac Ruvuza <ruvuzaisaac@gmail.com>
2023-07-13 11:19:46 +0300

    create destination directory, if non-existent

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -6,46 +6,46 @@ end
 
 -- check if we need to reload the file when it's changed {{{
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-  group = augroup("checktime"),
+  group = augroup "checktime",
   command = "checktime",
 })
 -- }}}
 
 -- resize splits if window got resized {{{
 vim.api.nvim_create_autocmd({ "VimResized" }, {
-  group = augroup("resize_splits"),
+  group = augroup "resize_splits",
   callback = function()
-    vim.cmd("tabdo wincmd =")
+    vim.cmd "tabdo wincmd ="
   end,
 })
 -- }}}
 
 -- highlight on yank {{{
 autocmd("TextYankPost", {
-  group = augroup("highlight_yank"),
+  group = augroup "highlight_yank",
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank { timeout = 40 }
   end,
 })
 -- }}}
 
 -- don't auto comment newlines {{{
 autocmd("BufEnter", {
-  group = augroup("auto_comment"),
-  command = "set fo-=c fo-=r fo-=o",
+  group = augroup "disableAutoComment",
+  command = "set formatoptions-=c formatoptions-=r formatoptions-=o",
 })
 -- }}}
 
 -- auto-remove trailing whitespace {{{
 autocmd("BufWritePre", {
-  group = augroup("whitespace"),
+  group = augroup "whitespace",
   command = "%s/\\s\\+$//e",
 })
 -- }}}
 
 -- go to the last location when opening a buffer {{{
 autocmd("BufReadPost", {
-  group = augroup("last_loc"),
+  group = augroup "last_loc",
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
@@ -58,7 +58,7 @@ autocmd("BufReadPost", {
 
 -- close some filetypes with <q> {{{
 autocmd("Filetype", {
-  group = augroup("close_with_q"),
+  group = augroup "close_with_q",
   callback = function(event)
     local filetype = vim.api.nvim_get_option_value("filetype", { buf = event.buf })
     local buftype = vim.api.nvim_get_option_value("buftype", { buf = event.buf })
@@ -71,7 +71,7 @@ autocmd("Filetype", {
 
 -- wrap and check for spell in text filetype {{{
 autocmd("Filetype", {
-  group = augroup("wrap_spell"),
+  group = augroup "wrap_spell",
   pattern = {
     "text",
     "gitcommit",
@@ -86,7 +86,7 @@ autocmd("Filetype", {
 
 -- terminal options {{{
 autocmd("TermOpen", {
-  group = augroup("terminal_opts"),
+  group = augroup "terminal_opts",
   callback = function()
     vim.opt_local.number = false
     vim.opt_local.relativenumber = false
@@ -94,11 +94,46 @@ autocmd("TermOpen", {
 })
 -- }}}
 
+-- don't list quickfix buffers {{{
 autocmd("FileType", {
-  desc = "Unlist quickfist buffers",
-  group = augroup("unlist_quickfist"),
+  -- group = augroup("unlist_quickfist"),
   pattern = "qf",
   callback = function()
     vim.opt_local.buflisted = false
   end,
 })
+-- }}}
+
+-- auto create dir when saving a file, in case intermediate directory doesn't exist {{{
+autocmd("BufWritePre", {
+  group = augroup "auto_create_dir",
+  callback = function(event)
+    if event.match:match "^%w%w+://" then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+-- }}}
+
+-- disable `mini.indentscope` on some filetypes {{{
+autocmd("Filetype", {
+  group = augroup "turn_off_indentscope",
+  pattern = {
+    "help",
+    "alpha",
+    "dashboard",
+    "neo-tree",
+    "Trouble",
+    "lazy",
+    "mason",
+    "notify",
+    "toggleterm",
+    "lazyterm",
+  },
+  callback = function()
+    vim.b.miniindentscope_disable = true
+  end,
+})
+-- }}}
