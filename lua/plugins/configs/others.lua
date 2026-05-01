b3b0822515bcac750be4327c1230c42ff958fc9a
Ruvuza Isaac <ruvuzaisaac@gmail.com>
2026-05-01 22:31:36 +0300

    gitsigns plugin

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -23,4 +23,45 @@ return {
   },
 
   -- gitsigns
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '\u{2590}' }, -- ▏
+        change = { text = '\u{2590}' }, -- ▐
+        delete = { text = '\u{2590}' }, -- ◦
+        topdelete = { text = '\u{25e6}' }, -- ◦
+        changedelete = { text = '\u{25cf}' }, -- ●
+        untracked = { text = '\u{25cb}' }, -- ○
+      },
+      signcolumn = true,
+      current_line_blame = false,
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end, { desc = 'Jump to next git [c]hange' })
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end, { desc = 'Jump to previous git [c]hange' })
+      end,
+    },
+  },
 }
