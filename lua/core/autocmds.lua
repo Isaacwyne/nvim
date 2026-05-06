c03eca8f922403ff19fc53ed818ff004b56bf48f
Ruvuza Isaac <ruvuzaisaac@gmail.com>
2026-05-06 21:31:11 +0300

    rules for specific file types (plain text files)

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,7 +1,8 @@
+local augroup = vim.api.nvim_create_augroup('UserConfig', { clear = true })
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
+  group = augroup,
   callback = function()
     vim.hl.on_yank()
   end,
@@ -9,13 +10,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- don't auto comment newlines
 vim.api.nvim_create_autocmd('BufEnter', {
-  group = vim.api.nvim_create_augroup('disableAutoComment', { clear = true }),
+  group = augroup,
   command = 'set formatoptions-=c formatoptions-=r formatoptions-=0',
 })
 
 -- go to the last location when opening a buffer {{{
 vim.api.nvim_create_autocmd('BufReadPost', {
-  group = vim.api.nvim_create_augroup('last_loc', { clear = true }),
+  group = augroup,
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
@@ -27,12 +28,23 @@ vim.api.nvim_create_autocmd('BufReadPost', {
 
 -- close some filetypes with <q> {{{
 vim.api.nvim_create_autocmd('Filetype', {
-  group = vim.api.nvim_create_augroup('close_with_q', { clear = true }),
+  group = augroup,
   callback = function(event)
     local filetype = vim.api.nvim_get_option_value('filetype', { buf = event.buf })
     local buftype = vim.api.nvim_get_option_value('buftype', { buf = event.buf })
     if buftype == 'nofile' or filetype == 'help' then
       vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true, nowait = true })
     end
+  end,
+})
+
+-- wrap, linebreak and spellcheck on markdown and text files
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup,
+  pattern = { 'markdown', 'text', 'gitcommit' },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.spell = true
   end,
 })
